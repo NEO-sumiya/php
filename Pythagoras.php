024c2e8ed2551034620a434264b6f93134e00eef
s-sumiya <s-sumiya@hg.locopo-jp.com>
2023-01-11 17:20:26 +0900

    add file Pythagoras

--- Pythagoras.php
+++ Pythagoras.php
@@ -5,9 +5,6 @@
 <?php
 
 	// 引数
-#テスト用引数
-#$_GET['int1'] = 3;
-#$_GET['int2'] = 4;
 	$input_int_a = $_GET['int1'];
 	$input_int_b = $_GET['int2'];
 
@@ -22,14 +19,14 @@
 	$input_int_c = sqrt($input_int_c2);
 
 	// 確認用msg
-	echo "■■■■■■■　確認用　■■■■■■■"."<br>\n";
-	echo "入力値辺a：".$input_int_a."<br>\n";
-	echo "入力値辺b：".$input_int_b."<br>\n";
-	echo "辺aの二乗：".$input_int_a2."<br>\n";
-	echo "辺bの二乗：".$input_int_b2."<br>\n";
-	echo "二乗加算 ：".$input_int_c2."<br>\n";
-	echo "辺c平方根：".$input_int_c."<br>\n";
-	echo "■■■■■■■　確認用　■■■■■■■"."<br>\n";
+	//echo "■■■■■■■　確認用　■■■■■■■"."<br>\n";
+	//echo "入力値辺a：".$input_int_a."<br>\n";
+	//echo "入力値辺b：".$input_int_b."<br>\n";
+	//echo "辺aの二乗：".$input_int_a2."<br>\n";
+	//echo "辺bの二乗：".$input_int_b2."<br>\n";
+	//echo "二乗加算 ：".$input_int_c2."<br>\n";
+	//echo "辺c平方根：".$input_int_c."<br>\n";
+	//echo "■■■■■■■　確認用　■■■■■■■"."<br>\n";
 
 	// テキスト出力
 	echo "<br>\n";
